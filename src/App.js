c31155ffbd6ca75a470c656e08fe72cc3da55252
GoncaloFig <goncalofigueiredowork@gmail.com>
2023-06-26 02:58:18 +0100

    coockies banner start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 import Game from './Components/Game'
+import { CookieBanner } from '@keepist/react-gdpr-cookie-banner';
+
 
 function App() {
   return (
+    <div>
+      <CookieBanner
+      message="Cookie banner message"
+      onAcceptPreferences = {() => { 
+        // load your preferences trackers here
+      }}
+      onAcceptStatistics = {() => {
+        // load your statistics trackers here
+      }}
+      onAcceptMarketing = {() => {
+        // load your marketing trackers here
+      }}
+      styles={{
+        // dialog: { backgroundColor: '#ffffff' },
+      }}
+    />
     <Game></Game>
+    </div>
   );
 }
 
